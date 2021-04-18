d6f421e97be648a385fca1fb27790343da140603
blueagain <rajat.joshi9994@gmail.com>
2021-04-18 17:33:35 +0530

    Created a SVC model which just runs endlessly

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -19,5 +19,15 @@ print(train_features.head())"""
 nlp = spacy.load('en_core_web_lg')
 # Disabling other pipes because we don't need them and it'll speed up this part a bit
 with nlp.disable_pipes():
-    doc_vectors = np.array([nlp(text) for text in train.tweet])
+    doc_vectors = np.array([nlp(text).vector for text in train.tweet])
 doc_vectors.shape
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(doc_vectors,
+                                                    train.tweet,
+                                                    test_size=0.15,
+                                                    random_state=1)
+from sklearn.svm import LinearSVC
+svc = LinearSVC(random_state=1, dual=False, max_iter=1000)
+svc.fit(X_train, y_train)
+print(f"Accuracy: {svc.score(X_test, y_test) * 100:.3f}%", )
