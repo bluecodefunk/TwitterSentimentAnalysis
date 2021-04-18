3d6023448ff3395e847c5f0256a6d230ba08fc19
blueagain <rajat.joshi9994@gmail.com>
2021-04-18 15:53:37 +0530

    Added new nlp tokenization

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,12 +1,23 @@
+import random
+
 import pandas as pd
 import numpy as np
 from feature_maker import FeatureCreation
+import spacy
+from random import sample
+import random
 
 # load data
 train = pd.read_csv("train_E6oV3lV.csv")
 test = pd.read_csv("test_tweets_anuFYb8.csv")
 # create features
-train_features = FeatureCreation(train,"tweet").feature_chain()
+"""train_features = FeatureCreation(train,"tweet").feature_chain()
 test_features = FeatureCreation(test,"tweet").feature_chain()
 
-train_features.head()
+print(train_features.head())"""
+
+nlp = spacy.load('en_core_web_lg')
+# Disabling other pipes because we don't need them and it'll speed up this part a bit
+with nlp.disable_pipes():
+    doc_vectors = np.array([nlp(text) for text in train.tweet])
+doc_vectors.shape
